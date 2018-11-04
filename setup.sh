ea95dd9f052d7c1c260bb9cfc4eec8f642b91b2d
yuvalm <yuvalm@iguazio.com>
2018-11-04 15:06:38 +0200

    added bash setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[ -d ${DIR}/venv ] && rm -rf ${DIR}/venv
+virtualenv -p python3 ${DIR}/venv
+source ${DIR}/venv/bin/activate
+
+pip install -r ${DIR}/requirements.txt
+pip install -e ${DIR}
