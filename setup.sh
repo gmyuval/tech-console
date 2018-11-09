301b6e158a0f6cc24fe2c84dfef193d41d65c232
Yuval Moran <yuvalg@gmail.com>
2018-11-09 11:41:39 +0200

    added python path exports to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export PYTHON_BIN_PATH="$(python3 -m site --user-base)/bin"
+export PATH="$PATH:$PYTHON_BIN_PATH"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 [[ -d ${DIR}/venv ]] && rm -rf ${DIR}/venv
